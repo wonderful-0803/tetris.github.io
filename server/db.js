c6c0a6ba7097858456d834484add13992b46cee2
wonderful-0803 <740909103@qq.com>
2021-06-07 00:32:12 +0800

    the second times

--- server/db.js
+++ server/db.js
@@ -1,10 +1,29 @@
-module.exports = {
-  mysql: {
-    host: 'localhost',
-    user: 'root',
-    password: '3.141592653', // mysql用户名密码
-    database: 'tetrisdb', // mysql数据库名
-    port: '3306' // mysql链接端口
-  }
+let mysql = require('mysql')
+exports.base = (sql, data, callback) => {
+    let connection = mysql.createConnection({
+        host: 'localhost', //本机连接不用改动
+        user: 'root', //root用户一般默认都有，可以不改
+        password: '3.141592653', //改成自己的密码
+        port: "3306",
+        database: 'tetrisdb' //改成自己的数据库名（注意数据库名和表名的区别）
+    })
+    connection.connect();
+    //创建数据库
+    connection.query("create table if not exists Login\
+     (`username` varchar(16) unique not null,`password` varchar(16) not null);", (error, results, fields) => {
+        if (error) throw error; {
+            console.log(error)
+        }
+        console.log(results)
+    })
+    connection.query(sql, data, (error, results, fields) => {
+        if (error) {
+            console.log(error)
+        }
+        console.log(results)
+        callback && callback(results, error)
+    })
+    connection.end();
 }
+
 
--- server/sqlMap.js
+++ server/sqlMap.js
@@ -1,9 +1,11 @@
-// sqlmap.js
+// sqlmap.js SQL语句映射文件，这里主要是对数据库的增改查操作。
 var sqlMap = {
   // home
   home: {
-    search: 'select * from zp_list',
-    add: 'insert into zp_list(name) values (?)'
+    search: 'select * from tetrisuser', // 查询语句一直有问题，node一直报错，目前还在找解决方案。
+    add: 'insert into tetrisuser (useId,username,userpwd) values (?,?,?)',
+    update: 'update tetrisuser set'
+    
   },
   other:{
     delete:'', // delete sql语句
